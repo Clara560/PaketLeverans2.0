0ab5544bfb24037b0d98b73446063118632ff315
Clara560 <clara.nygard@edu.edugrade.se>
2024-04-19 14:32:15 +0200

    project setup

---
+++ Examensarbete_paketLeverans/src/main/java/com/examensarbete_paketleverans/ExamensarbetePaketLeveransApplication.java
@@ -0,0 +1,13 @@
+package com.examensarbete_paketleverans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamensarbetePaketLeveransApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamensarbetePaketLeveransApplication.class, args);
+    }
+
+}
